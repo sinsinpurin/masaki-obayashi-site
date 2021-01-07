1aebf7c3bcbc07d3f72013ce843418fb01686ef0
sinsinpurin <tusanch6@gmail.com>
2021-01-08 05:33:02 +0900

    fix dev page size

--- src/Contents/development/Ethwal.js
+++ src/Contents/development/Ethwal.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Ethwal.css'
-import { Container, Row } from 'react-bootstrap'
 
 class Ethwal extends React.Component {
     render() {
@@ -8,23 +7,28 @@ class Ethwal extends React.Component {
             <>
                 <div className="Ethwal">
                     <div className="ethwal-content">
-                        <Container>
-                            <Row>
-                                <img src={require("../../Assets/Ethwalgen.png")} alt={"EthwalgenImg"} className="ethwal-img"></img>
+                        <ul className="eth-list-content">
+                            <li>
                                 <div className="ethwal-text">
                                     <div className="ethwal-title">
-                                        Ethereum Wallet Generator
-                                        </div>
+                                        Ethereum Wallet <br />
+                                            Generator
+                                    </div>
                                     <div className="ethwal-detail">
                                         Ethereumの秘密鍵、公開鍵、アドレスを <br />
-                                            1~100個生成するcliツール <br /><br />
+                                                1~100個生成するcliツール <br /><br />
                                     </div>
                                     <div className="ethwal-detail">
                                         <a href={"https://pypi.org/project/eth-wallet-generater/"}>Ethereum Wallet Generator</a>
                                     </div>
                                 </div>
-                            </Row>
-                        </Container>
+                            </li>
+                            <li>
+                                <div className="ethwal-img-container">
+                                    <img src={require("../../Assets/Ethwalgen.png")} alt={"EthwalgenImg"} className="ethwal-img"></img>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </>
--- src/Contents/development/Wantgo.js
+++ src/Contents/development/Wantgo.js
@@ -7,25 +7,35 @@ class Wantgo extends React.Component {
             <>
                 <div className="wantgo">
                     <div className="wantgo-content">
-                        <img src={require("../../Assets/wantgo.png")} alt={"wantgo"} className="wantgo-img"></img>
-                        <div className="wantgo-text">
-                            <div className="wantgo-title">
-                                Wantgo
-                            </div>
-                            <div className="wantgo-detail">
-                                たまたま見つけたお店<br />
-                                行きたいって思うけど<br />
-                                そのまま忘れてしまったりすることが... <br />
-                                <br />
-                                いつでも気軽に行きたいところをメモして、<br />
-                                旅行のときの予定を立てやすくしたり <br />
-                                今度行きたいところを共有したり<br />
-                                行きたい場所、やりたいことを<br />
-                                家族、彼女、友達と気軽に共有できるアプリ<br />
-                                <br />
-                                現在、開発中<br />
-                            </div>
-                        </div>
+                        <ul className="list-content">
+                            <li>
+                                <div>
+                                    <img src={require("../../Assets/wantgo.png")} alt={"wantgo"} className="wantgo-img"></img>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="wantgo-text">
+                                    <div className="wantgo-title">
+                                        WantGo
+                                    </div>
+                                    <div className="wantgo-detail">
+                                        <div className="wantgo-sub-detail">
+                                            たまたま見つけたお店 行きたいって思うけど<br />
+                                            そのまま忘れてしまったりすることが... <br />
+                                            <br />
+                                            いつでも気軽に行きたいところをメモして、<br />
+                                            旅行のときの予定を立てやすくしたり <br />
+                                            今度行きたいところを共有したり<br />
+                                        </div>
+                                        行きたい場所 <br />
+                                        やりたいことを<br />
+                                        家族、彼女、友達と気軽に共有できるアプリ<br />
+                                        <br />
+                                        現在、開発中<br />
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </>
